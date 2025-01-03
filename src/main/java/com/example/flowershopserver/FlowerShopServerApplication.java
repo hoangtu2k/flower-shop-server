c5d33048da403007e8d747312ec8a75b1b261bd6
hoangtu2k <thocodeonline@gmail.com>
2025-01-03 22:00:28 +0700

    create project

---
+++ src/main/java/com/example/flowershopserver/FlowerShopServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.flowershopserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowerShopServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowerShopServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/flowershopserver/FlowerShopServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.flowershopserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowerShopServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
